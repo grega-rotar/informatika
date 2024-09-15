c344e78997ccdaf72e07495ecd75fb48bebf3a8d
Grega Rotar <grega@etiam.si>
2024-09-15 12:49:52 +0200

    not tested

--- src/roleta/prov.cpp
+++ src/roleta/prov.cpp
@@ -1,5 +1,6 @@
 #include "config.h"
 #include "const.h"
+#include "esp_err.h"
 #include "esp_log.h"
 #include "nvs.h"
 
@@ -11,6 +12,8 @@ String prov_device_name = PROV_DEVICE_NAME;
 
 const char *prov_service_key = NULL; // Password for SofAP method, NOT USED
 
+typedef void (*SetString)(string);
+
 class ProvDetails {
       private:
         String pop;
@@ -18,176 +21,192 @@ class ProvDetails {
 
       public:
         ProvDetails(String p, String d) {
-                pop = pop;
+                pop = p;
                 device_name = d;
         }
         void setPop(String p) { pop = p; }
-        String getPop() { return pop; }
+        char *getPop() {
+                unsigned char bufferSize = pop.length() + 1;
+                char *buffer = new char[bufferSize];
+                pop.toCharArray(buffer, bufferSize);
+                return buffer;
+        }
 
         void setDeviceName(String d) { device_name = d; }
-        String getDeviceName() { return device_name; }
+        char *getDeviceName() {
+                unsigned char bufferSize = device_name.length() + 1;
+                char *buffer = new char[bufferSize];
+                device_name.toCharArray(buffer, bufferSize);
+                return buffer;
+        }
 };
 
+// setting provDetails to default values
 ProvDetails provDetails(PROV_POP, PROV_DEVICE_NAME);
 
-bool logAndCheckErr(esp_err_t err) {
+// function to log errors
+// DEPRECATED: it's not so useful
+bool log_and_check_err(esp_err_t err) {
         if (err == ESP_OK)
                 return false;
         ESP_LOGE(TAG, "NVS error: %s", esp_err_to_name(err));
         return true;
 }
 
-void getProvDetails(ProvDetails *prov) {
-        esp_err_t err = nvs_flash_init();
-        if (err == ESP_ERR_NVS_NO_FREE_PAGES ||
-            err == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+void init_nvs(esp_err_t *err) {
+        *err = nvs_flash_init();
+
+        // check the initalization errors
+        // ESP_ERR_NVS_NO_FREE_PAGES: No free pages available in the NVS
+        // partition. ESP_ERR_NVS_NEW_VERSION_FOUND: A new version of the NVS
+        // library has been detected, indicating compatibility issues.
+        if (*err == ESP_ERR_NVS_NO_FREE_PAGES ||
+            *err == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+                // earse the NVS partition to clear corrupted data
                 ESP_ERROR_CHECK(nvs_flash_erase());
-                err = nvs_flash_init();
+
+                // trying to initalize again
+                *err = nvs_flash_init();
         }
-        ESP_ERROR_CHECK(err);
-
-        nvs_handle_t nvs_handle;
-        err = nvs_open("prov", NVS_READONLY, &nvs_handle);
-        logAndCheckErr(err);
-
-        uint32_t pop = 0;
-        err = nvs_get_u32(nvs_handle, "pop", &pop);
-        logAndCheckErr(err);
-        prov_pop = PROV_PREFIX + String(pop, HEX);
-
-        uint32_t device_name = 0;
-        err = nvs_get_u32(nvs_handle, "dn", &device_name);
-        logAndCheckErr(err);
-        prov_device_name = String(device_name, HEX);
-
-        nvs_close(nvs_handle);
+        ESP_ERROR_CHECK(*err);
 }
 
-bool getAndCheckProvDetails(String &ppop, String &pdevice_name) {
-        esp_err_t err = nvs_flash_init();
-        if (err == ESP_ERR_NVS_NO_FREE_PAGES ||
-            err == ESP_ERR_NVS_NEW_VERSION_FOUND) {
-                ESP_ERROR_CHECK(nvs_flash_erase());
-                err = nvs_flash_init();
-        }
-        ESP_ERROR_CHECK(err);
-
-        nvs_handle_t nvs_handle;
-        err = nvs_open("prov", NVS_READONLY, &nvs_handle);
-        if (logAndCheckErr(err)) {
-                return false;
-        }
-
-        uint32_t pop = 0;
-        err = nvs_get_u32(nvs_handle, "pop", &pop);
-        logAndCheckErr(err);
-        ppop = PROV_PREFIX + String(pop, HEX);
-
-        uint32_t device_name = 0;
-        err = nvs_get_u32(nvs_handle, "dn", &device_name);
-        logAndCheckErr(err);
-        pdevice_name = String(device_name, HEX);
-
-        nvs_close(nvs_handle);
-
-        return (pop != 0 && device_name != 0);
-}
-
-void initalizeProvDetails() {
-
-        // checking if prov details have been created and saved
-        if (getAndCheckProvDetails(prov_pop, prov_device_name)) {
-                // prov details exist
-                ESP_LOGI(TAG, "prov details exist", "");
+// FIXME: add option to open as READ_ONLY
+void open_nvs_namespace(const char *namespace, nvs_handle_t *nvs_handle,
+                        esp_err_t *err) {
+        *err = nvs_open(namespace, NVS_READWRITE, nvs_handle);
+        if (*err != ESP_OK) {
+                ESP_LOGE(TAG, "nvs_open error: %s", esp_err_to_name(*err));
+                nvs_close(nvs_handle);
                 return;
         }
+}
 
-        // creating new prov credentials
-        esp_err_t err = nvs_flash_init();
-        if (err == ESP_ERR_NVS_NO_FREE_PAGES ||
-            err == ESP_ERR_NVS_NEW_VERSION_FOUND) {
-                ESP_ERROR_CHECK(nvs_flash_erase());
-                err = nvs_flash_init();
-        }
-        ESP_ERROR_CHECK(err);
+void init_prov_details(ProvDetails *pProvDetails) {
+        esp_err_t err;
+        init_nvs(&err);
 
         // opening "prov" namespace
         nvs_handle_t nvs_handle;
-        err = nvs_open("prov", NVS_READWRITE, &nvs_handle);
-        logAndCheckErr(err);
+        open_nvs_namespace("prov", &nvs_handle, &err);
 
-        // getting random 32 bit integars
+        // generating and saving data to NVS
         uint32_t pop = esp_random();
-        uint32_t device_name = esp_random();
         err = nvs_set_u32(nvs_handle, "pop", pop);
-        logAndCheckErr(err);
+        if (err != ESP_OK) {
+                nvs_close(nvs_handle);
+                return;
+        }
+        pProvDetails->setPop(String(pop, HEX));
+
+        uint32_t device_name = esp_random();
         err = nvs_set_u32(nvs_handle, "dn", device_name);
-        logAndCheckErr(err);
+        if (err != ESP_OK) {
+                nvs_close(nvs_handle);
+                return;
+        }
+        pProvDetails->setDeviceName(PROV_PREFIX + String(device_name, HEX));
 
         // Commit written value to NVS storage
         err = nvs_commit(nvs_handle);
-        logAndCheckErr(err);
+        log_and_check_err(err);
 
         // closing NVS
         nvs_close(nvs_handle);
+        return;
+}
+
+void handle_prov_nvs_err(esp_err_t err, ProvDetails *pProvDetails) {
+        if (err == ESP_OK) {
+                return;
+        };
+
+        if (err == ESP_ERR_NVS_NOT_FOUND) {
+                // prov details don't exist in NVS
+                // creating new ones
+                init_prov_details(pProvDetails);
+        }
+}
+
+void ensure_prov_details(ProvDetails *pProvDetails) {
+        // FIXME: add error handling
+        // Attempt to initalize the NVS (Non-Voltaile Storage) flash
+        esp_err_t err;
+        init_nvs(&err);
+
+        // opening "prov" namespace
+        nvs_handle_t nvs_handle;
+        open_nvs_namespace("prov", &nvs_handle, &err);
+
+        uint32_t pop = 0;
+        err = nvs_get_u32(nvs_handle, "pop", &pop);
+        pProvDetails->setPop(String(pop, HEX));
+        // check if data does not exist and creating new
+        handle_prov_nvs_err(err, pProvDetails);
+
+        uint32_t device_name = 0;
+        err = nvs_get_u32(nvs_handle, "dn", &device_name);
+        pProvDetails->setDeviceName(String(device_name, HEX));
+        // check if data does not exist and creating new
+        handle_prov_nvs_err(err, pProvDetails);
+
+        nvs_close(nvs_handle);
+        return;
 }
 
 void SysProvEvent(arduino_event_t *sys_event) {
         switch (sys_event->event_id) {
-        case ARDUINO_EVENT_WIFI_STA_GOT_IP:
-                Serial.print("\nConnected IP address : ");
-                Serial.println(
-                    IPAddress(sys_event->event_info.got_ip.ip_info.ip.addr));
-                break;
-        case ARDUINO_EVENT_WIFI_STA_DISCONNECTED:
-                Serial.println(
-                    "\nDisconnected. Connecting to the AP again... ");
-                break;
-        case ARDUINO_EVENT_PROV_START:
-                Serial.println(
-                    "\nProvisioning started\nGive Credentials of your access "
-                    "point using smartphone app");
-                break;
-        case ARDUINO_EVENT_PROV_CRED_RECV: {
-                Serial.println("\nReceived Wi-Fi credentials");
-                Serial.print("\tSSID : ");
-                Serial.println(
-                    (const char *)sys_event->event_info.prov_cred_recv.ssid);
-                Serial.print("\tPassword : ");
-                Serial.println((char const *)sys_event->event_info
-                                   .prov_cred_recv.password);
-                break;
-        }
-        case ARDUINO_EVENT_PROV_CRED_FAIL: {
-                Serial.println(
-                    "\nProvisioning failed!\nPlease reset to factory and retry "
-                    "provisioning\n");
-                if (sys_event->event_info.prov_fail_reason ==
-                    WIFI_PROV_STA_AUTH_ERROR)
-                        Serial.println("\nWi-Fi AP password incorrect");
-                else
-                        Serial.println("\nWi-Fi AP not found....Add API \" "
-                                       "nvs_flash_erase() \" "
-                                       "before beginProvision()");
-                break;
-        }
-        case ARDUINO_EVENT_PROV_CRED_SUCCESS:
-                Serial.println("\nProvisioning Successful");
-                break;
-        case ARDUINO_EVENT_PROV_END:
-                Serial.println("\nProvisioning Ends");
-                break;
-        default:
-                break;
+                case ARDUINO_EVENT_WIFI_STA_GOT_IP:
+                        Serial.print("\nConnected IP address : ");
+                        Serial.println(IPAddress(
+                            sys_event->event_info.got_ip.ip_info.ip.addr));
+                        break;
+                case ARDUINO_EVENT_WIFI_STA_DISCONNECTED:
+                        Serial.println(
+                            "\nDisconnected. Connecting to the AP again... ");
+                        break;
+                case ARDUINO_EVENT_PROV_START:
+                        Serial.println("\nProvisioning started\nGive "
+                                       "Credentials of your access "
+                                       "point using smartphone app");
+                        break;
+                case ARDUINO_EVENT_PROV_CRED_RECV: {
+                        Serial.println("\nReceived Wi-Fi credentials");
+                        Serial.print("\tSSID : ");
+                        Serial.println((const char *) sys_event->event_info
+                                           .prov_cred_recv.ssid);
+                        Serial.print("\tPassword : ");
+                        Serial.println((char const *) sys_event->event_info
+                                           .prov_cred_recv.password);
+                        break;
+                }
+                case ARDUINO_EVENT_PROV_CRED_FAIL: {
+                        Serial.println("\nProvisioning failed!\nPlease reset "
+                                       "to factory and retry "
+                                       "provisioning\n");
+                        if (sys_event->event_info.prov_fail_reason ==
+                            WIFI_PROV_STA_AUTH_ERROR)
+                                Serial.println("\nWi-Fi AP password incorrect");
+                        else
+                                Serial.println(
+                                    "\nWi-Fi AP not found....Add API \" "
+                                    "nvs_flash_erase() \" "
+                                    "before beginProvision()");
+                        break;
+                }
+                case ARDUINO_EVENT_PROV_CRED_SUCCESS:
+                        Serial.println("\nProvisioning Successful");
+                        break;
+                case ARDUINO_EVENT_PROV_END:
+                        Serial.println("\nProvisioning Ends");
+                        break;
+                default:
+                        break;
         }
 }
 
 void setupProvisioning() {
-        bool isProvisioned = false;
-        esp_err_t error = wifi_prov_mgr_is_provisioned(&isProvisioned);
-        ESP_LOGE(TAG, "Is provisioned err: %s", esp_err_to_name(error));
-        ESP_LOGE(TAG, "Is provisioned err: %b", isProvisioned);
-        initalizeProvDetails();
+        ensure_prov_details(&provDetails);
 
         // assigning WiFi event function
         WiFi.onEvent(SysProvEvent);
@@ -198,8 +217,9 @@ void setupProvisioning() {
                             0xea, 0x4a, 0x82, 0x03, 0x04, 0x90, 0x1a, 0x02};
         WiFiProv.beginProvision(
             WIFI_PROV_SCHEME_BLE, WIFI_PROV_SCHEME_HANDLER_FREE_BTDM,
-            WIFI_PROV_SECURITY_1, prov_pop.c_str(), prov_device_name.c_str(),
-            prov_service_key, uuid, false);
+            WIFI_PROV_SECURITY_1, provDetails.getPop(),
+            provDetails.getDeviceName(), prov_service_key, uuid, false);
         log_d("ble qr");
-        WiFiProv.printQR(prov_device_name.c_str(), prov_pop.c_str(), "ble");
+        WiFiProv.printQR(provDetails.getDeviceName(), provDetails.getPop(),
+                         "ble");
 }
